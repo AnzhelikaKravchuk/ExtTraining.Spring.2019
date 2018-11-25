866a5aed6c2ddb5998b0e8d320e0c4c2cccaca0e
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2018-11-25 13:36:31 +0300

    Delete WeatherInfo.cs

--- No2/WeatherInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace No2
-{
-    public class WeatherInfo
-    {
-        public int Temperature { get; set; }
-        public int Humidity { get; set; }
-        public int Pressure { get; set; }
-    }
-}
